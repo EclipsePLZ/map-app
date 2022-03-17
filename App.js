08c082b3027f896d6a535faec133dd57252a2e78
Boris Pavlov <borzpavlov@gmail.com>
2022-03-17 18:48:49 +0500

    add map

--- App.js
+++ App.js
@@ -1,12 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useContext} from 'react';
+import { StyleSheet, Text, View, Button, Dimensions} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapView, { Marker } from 'react-native-maps';
 
-export default function App() {
+var counter = 1;
+const initialMarkerCoords = {0: {
+  latitude: 58.006948,
+  longitude: 56.238385,
+  },
+1: {
+  latitude: 58.056948,
+  longitude: 56.248385,
+  }
+}
+
+
+function HomeScreen({ navigation }) {
+  const startRegion = {
+    latitude: 58.006948,
+    longitude: 56.238385,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }
+  const [markerCoords,setMarkerCoords] = useContext(MyContext);
+
+  const addMarker = (e) => {
+    let newMarker = {
+      ...e.nativeEvent.coordinate
+    }
+    counter++;
+    let res = {...markerCoords, [counter]:newMarker};
+    setMarkerCoords(res)
+  }
+
+  const markerList = Object.values(markerCoords).map((x,i)=> <Marker key={i} coordinate={x} title="Title1" onPress={e=>{
+    navigation.navigate('Details')
+  }
+  }></Marker>)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} initialRegion={startRegion} onPress={addMarker}>
+        {markerList}
+      </MapView>
     </View>
+  );
+}
+
+function DetailsScreen({ route, navigation }) {
+  const [markerCoords,setMarkerCoords] = useContext(MyContext);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen {JSON.stringify(markerCoords[0])}</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+const MyContext = React.createContext();
+export default function App() {
+  const [state, setState] = useState(initialMarkerCoords);
+  return (
+    <MyContext.Provider value={[state, setState]}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </MyContext.Provider>
   );
 }
 
@@ -16,5 +79,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
